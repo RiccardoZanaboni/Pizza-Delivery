a12ea81b83e37afd849cccb4cce2f8422503f91a
RiccardoZanaboni <riccardo.zanaboni02@universitadipavia.it>
2019-03-26 23:59:40 +0100

    aggiunto libreria sql

--- Progetto_db/src/Database.java
+++ Progetto_db/src/Database.java
@@ -1,2 +1,22 @@
+import java.sql.*;
+
 public class Database {
+    public Database(){}
+
+    public void connectdatabase(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/singleschool","root","ilrichi10");
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("select * from dipart");
+            while (rs.next()) {
+                //
+            }
+            statement.close();
+            con.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
 }
