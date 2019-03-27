6f4ece29f85e59d4cf427dd48b49989159b6ed3a
RiccardoZanaboni <riccardo.zanaboni02@universitadipavia.it>
2019-03-27 17:45:04 +0100

    Prova db

--- Progetto_db/src/Database.java
+++ Progetto_db/src/Database.java
@@ -5,10 +5,10 @@ public class Database {
     public static void main(String[] args) {
 
             try {
-                Class.forName("com.mysql.jdbc.Driver");
+                Class.forName("com.mysql.cj.jdbc.Driver");
                 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/PROVA", "root", "ilrichi10");
                 Statement statement = con.createStatement();
-                ResultSet rs = statement.executeQuery("select * from DIPART");
+                ResultSet rs = statement.executeQuery("select * from DIPENDENTI");
                 while (rs.next()) {
                     System.out.println(rs.getInt(1) + " " + rs.getString(2)+ " " + rs.getString(3));
                 }
