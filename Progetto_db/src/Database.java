f378fec5fb6e2913d8d51b5ba2cdc88b7101a5fe
RiccardoZanaboni <riccardo.zanaboni02@universitadipavia.it>
2019-03-28 12:47:28 +0100

    Prova db for all

--- Progetto_db/src/Database.java
+++ Progetto_db/src/Database.java
@@ -7,8 +7,8 @@ public class Database {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/PIZZERIA", "root", "ilrichi10");
-                //PreparedStatement preparedStmt = con.prepareStatement("INSERT INTO PIZZERIA.Utenti VALUES ('MUSI',10)");
-                //preparedStmt.execute();
+                PreparedStatement preparedStmt = con.prepareStatement("INSERT INTO PIZZERIA.Utenti VALUES ('MUSI',10)");
+                preparedStmt.execute();
                 Statement statement = con.createStatement();
                 ResultSet rs = statement.executeQuery("select * from PIZZERIA.Utenti");
                 while (rs.next()) {
