bbba20742b5a6a8d84e0a0b5998c1c45b0f990c9
RiccardoZanaboni <riccardo.zanaboni02@universitadipavia.it>
2019-04-10 23:41:58 +0200

    Iniziato a creare classi con attributi

---
+++ Progetto_db/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Pizzeria WolfofPizza=new Pizzeria("WolfofPizza","via de Gasperi 5",new Date(2019,0,1,19,0),new Date(2019,0,31,23,0,0));
+
+    }
+}
