1e31c45c6b8528b696ec6340d440d7813425a960
RiccardoZanaboni <riccardo.zanaboni02@universitadipavia.it>
2019-04-11 15:36:53 +0200

    Iniziato a creare classi con attributi

--- Progetto/src/Order.java
+++ Progetto/src/Order.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Order {
     private String codice;
     private String indirizzo;
     private Date orario;
+    private ArrayList <Pizza> pizzeordinate;
+
 
     public Order(String codice, String indirizzo, Date orario) {
         this.codice = codice;
         this.indirizzo = indirizzo;
         this.orario = orario;
+        this.pizzeordinate=new ArrayList<>();
+    }
+
+    public void AddPizza(Pizza pizza){
+        pizzeordinate.add(pizza);
     }
 }
--- Progetto/src/Pizzeria.java
+++ Progetto/src/Pizzeria.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 
@@ -8,14 +9,24 @@ public class Pizzeria {
     private Date orario_Apertura;
     private Forno infornate[];
     private DeliveryMan fattorini_tempi[];
+    private int numordine;
+    private ArrayList <Order> ordini;
 
-    public Pizzeria(String nome, String indirizzo,Date orario_Apertura, Date orario_Chiusura) {
+    public Pizzeria(String nome, String indirizzo, Date orario_Apertura, Date orario_Chiusura) {
         this.nome = nome;
         this.indirizzo = indirizzo;
         this.orario_Chiusura = orario_Chiusura;
         this.orario_Apertura = orario_Apertura;
         this.infornate = new Forno[12 * (orario_Chiusura.getHours() - orario_Apertura.getHours())];
         this.fattorini_tempi = new DeliveryMan[6 * (orario_Chiusura.getHours() - orario_Apertura.getHours())];
+        this.ordini=new ArrayList<>();
+    }
+
+    public void makeorder(String indirizzo,String username,Date orario){
+        String codice="cod"+Math.random()+numordine;
+        numordine++;
+        Order or=new Order(codice,indirizzo,orario);
+        ordini.add(or);
     }
 
     public Date getOrario_Chiusura() {
