a42c06cb8f70d353e8e8ba53e6ef5e72c04558d3
FeBD8 <48360582+FeBD8@users.noreply.github.com>
2019-04-11 15:44:20 +0200

    Update Main.java

--- Progetto/src/Main.java
+++ Progetto/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Pizzeria WolfOfPizza = new Pizzeria("Wolf Of Pizza","Via de Gasperi 5, Pavia", new Date(2019,0,1,19,0),new Date(2019,0,31,23,0,0));
+        Pizzeria wolfOfPizza = new Pizzeria("Wolf Of Pizza","Via de Gasperi 5, Pavia", new Date(2019,0,1,19,0),new Date(2019,0,31,23,0,0));
 
     }
 }
