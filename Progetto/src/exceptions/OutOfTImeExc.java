30f644f4548b202427c2a8a153de0927c0f4945d
fetchubuntu <andrea.fecchio01@universitadipavia.it>
2019-04-20 16:54:39 +0200

    sistemate eccezioni della Pizzeria e dovrebbe funzionare

--- Progetto/src/exceptions/OutOfTImeExc.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class OutOfTImeExc extends Exception {
-    public OutOfTImeExc(){
-        super("Ora non valida"); // usata per evitare chiamata ricorsiva pericolosa che dà errore
-    }
-
-}
