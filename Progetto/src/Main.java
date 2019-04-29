3f55af614b109242ad672b2022a2618e14cf96cf
francescomusitano <francesco.musitano02@universitadipavia.it>
2019-04-29 20:25:04 +0200

    Sistemato Merge con Zana, restano problemi quando torni indietro

--- Progetto/src/Main.java
+++ Progetto/src/Main.java
@@ -6,18 +6,18 @@
 
 public class Main {
     public static void main(String[] args) {
-        Pizzeria wolfOfPizza = new Pizzeria("Wolf Of PizzaMenu","Via de Gasperi 5, Pavia", new Date(2019,0,1,19,0),new Date(2019,0,31,23,0,0));
+      //  Pizzeria wolfOfPizza = new Pizzeria("Wolf Of PizzaMenu","Via de Gasperi 5, Pavia", new Date(2019,0,1,19,0),new Date(2019,0,31,23,0,0));
 
 
         //wolfOfPizza.AddFattorino(new DeliveryMan("Marco",wolfOfPizza));
-        wolfOfPizza.AddFattorino(new DeliveryMan("Musi",wolfOfPizza));
+       // wolfOfPizza.AddFattorino(new DeliveryMan("Musi",wolfOfPizza));
 
-        wolfOfPizza.ApriPizzeria(8);
+        //wolfOfPizza.ApriPizzeria(8);
         //wolfOfPizza.makeOrder();
-        wolfOfPizza.makeOrder(); //utilizzare due make order per testare vettore data
+       //utilizzare due make order per testare vettore data
         //wolfOfPizza.makeOrder();
         //System.out.print(wolfOfPizza.stampaMenu()+"\n");
         //System.out.print(napoli.getDescrizione()+"\n");
         //System.out.print(cotto.getDescrizione());
     }
-}*/
+}
