9fcf3e79040664e2fed126e0ae0b0c843ff6f42c
fetchubuntu <andrea.fecchio01@universitadipavia.it>
2019-05-11 15:19:45 +0200

    Funziona il bottone RimuoviIngrediente; create classi apposta per bottoni di aggiunta/rimozione

--- Progetto/src/GraphicInterface.java
+++ Progetto/src/GraphicInterface.java
@@ -22,7 +22,7 @@ public class GraphicInterface extends Application{
   Button button;
   Scene scene1, scene2, scene3;
   TableView<Pizza> table;
-  Pizzeria wolf = new Pizzeria("Wolf Of pizzeria.Pizza","Via Bolzano 10, Pavia", new Date(2019,0,1,19,0),new Date(2019,0,31,23,0,0));
+  Pizzeria wolf = new Pizzeria("Wolf Of Pizza","Via Bolzano 10, Pavia", new Date(2019,0,1,19,0),new Date(2019,0,31,23,0,0));
   int tot=0;
 
   public static void main(String[] args) { launch(args); }
@@ -86,7 +86,7 @@ public class GraphicInterface extends Application{
     scene1 = new Scene(layout, 600, 600);
     window.setResizable(false);
     window.setScene(scene1);
-    window.setTitle("Wolf of pizzeria.Pizza");
+    window.setTitle("Wolf of Pizza");
     window.getIcons().add(new javafx.scene.image.Image("elementiGrafici/wolf_pizza.jpg"));
     window.show();
 
