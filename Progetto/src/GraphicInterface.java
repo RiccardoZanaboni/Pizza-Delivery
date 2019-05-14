e244699650c5895627b4fb41870f384ce929aa5e
matteo <matteo.gobbo12@gmail.com>
2019-05-14 21:54:35 +0200

    Modifica recap ordine, ma problema con visualizzazione bottoni. Inserimento di una pagina di apertura

--- Progetto/src/GraphicInterface.java
+++ Progetto/src/GraphicInterface.java
@@ -1,34 +1,76 @@
 import elementiGrafici.MenuPage;
 import elementiGrafici.OrderPage1;
 import elementiGrafici.OrderPage2;
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
+import javafx.util.Duration;
 import pizzeria.DeliveryMan;
 import pizzeria.Order;
 import pizzeria.Pizza;
 import pizzeria.Pizzeria;
 
+import java.awt.*;
 import java.util.Date;
 
 // TODO MIGLIORARE GRAFICA CON COLORI E IMMAGINI
 
 public class GraphicInterface extends Application {
 	Stage window;
-	Scene scene3;
+	Scene scene0, scene3;
 	Pizzeria wolf = new Pizzeria("Wolf Of Pizza","Via Bolzano 10, Pavia", new Date(2019,0,1,19,0),new Date(2019,0,31,23,0,0));
 
 	public static void main(String[] args) { launch(args); }
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+
 		window = primaryStage;
 
-		MenuPage menuPage = new MenuPage();
-		menuPage.display(window, scene3, wolf);
+		Image image = new Image("elementiGrafici/wolf_pizza.png");
+		ImageView imageView = new ImageView(image);
+		imageView.setFitHeight(600);
+		imageView.setFitWidth(600);
+
+
+		//Label label = new Label("Ciao");
+
+		StackPane stackPane = new StackPane();
+		stackPane.getChildren().addAll(imageView);
+		//label.setStyle("-fx-font: 24 arial");
+		//imageView.setImage(image);
+
+		scene0 = new Scene(stackPane, 600, 600);
+		window.setScene(scene0);
+		window.show();
+		FadeTransition fadeIn = new FadeTransition(Duration.seconds(4), stackPane);
+		fadeIn.setFromValue(0);
+		fadeIn.setToValue(1);
+		fadeIn.setCycleCount(1);
+
+		//Finish splash with fade out effect
+		FadeTransition fadeOut = new FadeTransition(Duration.seconds(2), stackPane);
+		fadeOut.setFromValue(1);
+		fadeOut.setToValue(0);
+		fadeOut.setCycleCount(1);
+
+		fadeIn.play();
+
+		fadeIn.setOnFinished((e) -> {
+			fadeOut.play();
+			MenuPage menuPage = new MenuPage();
+			menuPage.display(window, scene3, wolf);
+		});
+
+
+
+
 	}
 }
